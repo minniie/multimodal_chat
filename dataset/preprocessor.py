6c64feebdb4a489c162df91a7d25507b0bcb490d
minniie <mlee981001@gmail.com>
2022-09-30 05:59:59 +0000

    [Python] <feat> #1 load data into image retriever format

--- dataset/preprocessor.py
+++ dataset/preprocessor.py
@@ -1,7 +1,11 @@
 import glob
 import json
+import requests
+import re
 
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
+
+from util.text import clean_uttr, remove_empty_uttr
 
 
 class PhotochatPreprocessor():
@@ -20,7 +24,7 @@ class PhotochatPreprocessor():
 
     def preprocess(self):
         file_path_list = sorted(glob.glob("dataset/photochat/*/**"))
-        for file_path in file_path_list:
+        for file_path in [file_path_list[-1]]:
             with open(file_path) as f:
                 data = json.load(f)
                 for datum in data:
@@ -30,30 +34,43 @@ class PhotochatPreprocessor():
                         curr_set = "dev_set"
                     elif "test" in file_path:
                         curr_set = "test_set"
-                    # TODO: dataset for image retriever
-                    # images = [Image.open(requests.get(url, stream=True).raw) for url in urls]
-                    self.data_for_image_retriever[curr_set].append(
-                        [uttr["message"] for uttr in datum["dialogue"]]
-                    )
 
-                    # dataset for response generator
-                    curr_dialog = []
-                    curr_user_id = -1
+                    # make dialog alternate btw two speakers
+                    dialog_alternate = []
                     for turn in datum["dialogue"]:
-                        if not turn["message"]:
-                            continue
-                        if curr_user_id == turn["user_id"]:
-                            curr_dialog[-1] += " " + turn["message"]
+                        if not dialog_alternate:
+                            dialog_alternate.append(turn)
                         else:
-                            curr_dialog.append(turn["message"])
-                        curr_user_id = turn["user_id"]
-                    for i in range(len(curr_dialog)):  
-                        self.data_for_response_generator[curr_set].append(curr_dialog[:i+1])
+                            if turn["user_id"] == dialog_alternate[-1]["user_id"]:
+                                dialog_alternate[-1]["message"] += " " + turn["message"] 
+                            else:
+                                dialog_alternate.append(turn)
+                        if turn["share_photo"]:
+                            share_photo_idx = len(dialog_alternate)-1
+                    dialog_alternate = [d["message"] for d in dialog_alternate]
+                    dialog_alternate = clean_uttr(dialog_alternate)
+                    
+                    # data for image retriever
+                    dialog_trunc = remove_empty_uttr(dialog_alternate[:share_photo_idx+1])
+                    try:
+                        image = Image.open(requests.get(datum["photo_url"], stream=True).raw)
+                    except UnidentifiedImageError: # 14 images no longer valid
+                        continue
+                    self.data_for_image_retriever[curr_set].append({
+                        "dialog": dialog_trunc, "image": image
+                    })
 
+                    # data for response generator
+                    dialog_alternate = remove_empty_uttr(dialog_alternate)
+                    dialog_per_uttr = [dialog_alternate[:i+1] for i in range(len(dialog_alternate))]
+                    self.data_for_response_generator[curr_set].extend(dialog_per_uttr)
+
+        print(f"{'*'*10} image retriever")
+        print(f"{'*'*5} train set: {len(self.data_for_image_retriever['train_set'])}")
+        print(f"{'*'*5} dev set: {len(self.data_for_image_retriever['dev_set'])}")
+        print(f"{'*'*5} test set: {len(self.data_for_image_retriever['test_set'])}")
+
+        print(f"{'*'*10} response generator")
         print(f"{'*'*5} train set: {len(self.data_for_response_generator['train_set'])}")
         print(f"{'*'*5} dev set: {len(self.data_for_response_generator['dev_set'])}")
         print(f"{'*'*5} test set: {len(self.data_for_response_generator['test_set'])}")
-
-
-# p = PhotochatPreprocessor()
-# p.preprocess()
--- util/text.py
+++ util/text.py
@@ -2,22 +2,23 @@ import re
 from typing import List
 
 
-def truncate_context(
-        context: List[str],
-        max_context_len: int
-    ):
+def truncate_context(context: List[str], max_context_len: int):
     return context[-max_context_len:]
 
 
-def join_context(
-        context: List[str],
-        sep: str
-    ):
+def join_context(context: List[str], sep: str):
     return sep.join(context)
 
 
-def split_context(
-        context: str,
-        sep: str
-    ):
+def split_context(context: str, sep: str):
     return context.split(sep)
+
+
+def clean_uttr(dialog: List[str]):
+    dialog = [re.sub(r'\s+', ' ', d).strip() for d in dialog]
+    return dialog
+
+
+def remove_empty_uttr(dialog: List[str]):
+    dialog = list(filter(None, dialog))
+    return dialog
