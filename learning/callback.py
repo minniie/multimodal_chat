93495fcc32e5171e103e9ba1e47e33be61adaa64
Min Lee <mlee981001@gmail.com>
2023-01-27 05:04:12 +0000

    [Python] <feat> #1 calculate PPl for BLIP

--- learning/callback.py
+++ learning/callback.py
@@ -1,3 +1,5 @@
+import math
+
 from transformers.integrations import TensorBoardCallback
 
 from util.metric import BLEU
@@ -6,11 +8,15 @@ from util.metric import BLEU
 class MetricCallback(TensorBoardCallback):
     
     def on_evaluate(self, args, state, control, **kwargs):
+        # calculate ppl
+        loss = state.log_history[-1]["eval_loss"]
+        ppl = math.exp(loss)
+        
+        # calculate bleu
         model = kwargs["model"]
         tokenizer = kwargs["tokenizer"]
         eval_dataloader = kwargs["eval_dataloader"]
         preds_text, labels_text = [], []
-        
         for inputs in eval_dataloader:
             inputs = {k: v.to(model.device) for k, v in inputs.items()}
             labels = inputs.pop("labels")
@@ -23,5 +29,7 @@ class MetricCallback(TensorBoardCallback):
             labels_text.extend(label_text)
         
         bleu = BLEU(preds_text, labels_text)
+
+        self.tb_writer.add_scalar("eval/ppl", ppl, state.global_step)
         self.tb_writer.add_scalar("eval/bleu-1", bleu["bleu-1"], state.global_step)
         self.tb_writer.add_scalar("eval/bleu-2", bleu["bleu-2"], state.global_step)
